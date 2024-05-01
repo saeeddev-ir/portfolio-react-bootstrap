54b58bc70145b35cedfa942be01e63f5759cf97f
Saeed Abdollahi <saeed.abdollahi7878@gmail.com>
2024-05-01 16:43:03 +0330

    add all essential files

---
+++ src/components/Newsletter.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
+
+export const Newsletter = () => {
+  return (
+    <Col lg={12}>
+      <div className="newsletter-bx wow slideInUp">
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            <h3>
+              Subscribe to our Newsletter<br></br> & Never miss latest updates
+            </h3>
+          </Col>
+          <Col md={6} xl={7}>
+            <form>
+              <div className="new-email-bx">
+                <input type="email" placeholder="Email Address" />
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  );
+};
